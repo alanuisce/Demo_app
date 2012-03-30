a8aebfd1c50053f51e844b99f9adda3fc5ebfb78
Alan Waters <alanuisce75@yahoo.ie>
2012-03-30 19:47:52 +0100

    Week 6

--- src/main/java/ie/cit/cloudapp/TodoServlet.java
+++ src/main/java/ie/cit/cloudapp/TodoServlet.java
@@ -6,10 +6,15 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
 public class TodoServlet extends HttpServlet {
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
@@ -18,62 +23,48 @@ public class TodoServlet extends HttpServlet {
 		doForward(req, resp);
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		TodoRepository repo = getRepo(req);
+		Todo todo = new Todo();
+		todo.setText(req.getParameter("text"));
+		repo.addTodo(todo);
+
+		req.setAttribute("todos", repo.getTodos());
+		doForward(req, resp);
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		Integer index = Integer.valueOf(req.getParameter("todoId"));
+		TodoRepository repo = getRepo(req);
+		Todo todo = repo.getTodos().get(index - 1);
+		todo.setDone(!todo.isDone());
+		req.setAttribute("todos", repo.getTodos());
+		doForward(req, resp);
+	}
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		Integer index = Integer.valueOf(req.getParameter("todoId"));
+		TodoRepository repo = getRepo(req);
+		repo.getTodos().remove(index - 1);
+		req.setAttribute("todos", repo.getTodos());
+		doForward(req, resp);
+	}
+
+	private TodoRepository getRepo(HttpServletRequest req) {
+		WebApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+		return ctx.getBean("repo", TodoRepository.class);
+	}
+
 	private void doForward(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		RequestDispatcher rd = getServletContext().getRequestDispatcher(
 				"/todo_list.jsp");
 		rd.forward(req, resp);
-	}
-	
-	@Override
-	protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		TodoRepository repo = getRepo(req);
-		repo.getTodos().remove(Integer.valueOf(req.getParameter("todoId")) - 1);
-		req.setAttribute("todos", repo.getTodos());
-		doForward(req, resp);
-	}
-	
-	
-	protected void doUpdate(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		TodoRepository repo = getRepo(req);
-		Todo todo = repo.getTodos().get(Integer.valueOf(req.getParameter("todoId")) - 1);
-		todo.setDone(!todo.isDone());
-		req.setAttribute("todos", repo.getTodos());
-		doForward(req, resp);
-	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		String methodParam = req.getParameter("_method");
-		if(methodParam != null && methodParam.equals("delete"))
-		{
-			doDelete(req, resp);
-		}
-		if(methodParam != null && methodParam.equals("put"))
-		{
-			doUpdate(req, resp);
-		}
-		if(methodParam != null && methodParam != "delete" && methodParam != "put")
-		{
-			TodoRepository repo = getRepo(req);
-			Todo todo = new Todo();
-			todo.setText(req.getParameter("text"));
-			repo.addTodo(todo);
-			req.setAttribute("todos", repo.getTodos());
-			doForward(req, resp);
-		}
-	}
-
-	private TodoRepository getRepo(HttpServletRequest req) {
-		HttpSession session = req.getSession(true);
-		TodoRepository repo = (TodoRepository) session.getAttribute("repo");
-		if (repo == null) {
-			repo = new TodoRepository();
-			session.setAttribute("repo", repo);
-		}
-		return repo;
 	}
 }
